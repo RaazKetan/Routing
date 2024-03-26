dfde9c6eaf353eb66aa38dd309e3b195dd833a96
Raaz <21ketanraaz@gmail.com>
2024-03-26 11:12:12 +0530

    Routing 2nd approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { createBrowserRouter,Route,RouterProvider } from 'react-router-dom'
 import HomePage from './pages/Home';
 import ProductPage from './pages/Products';
 import './App.css'
 
-const router = createBrowserRouter([
-  {path: '/', element: <HomePage />},
-  {path: '/products', element: <ProductPage/>},
-])
 
+const routeDefinations = createBrowserRouter(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductPage />} />
+  </Route>
+);
+// const router = createBrowserRouter([
+//   {path: '/', element: <HomePage />},
+//   {path: '/products', element: <ProductPage/>},
+// ])
+const router = createBrowserRouter(routeDefinations)
 function App() {
   return (
     <RouterProvider router={router}/>
