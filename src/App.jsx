a28842e03e3d19f58e7ed3bb68698edd1fdd86f3
Raaz <21ketanraaz@gmail.com>
2024-03-26 12:01:17 +0530

    Adding Error Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,23 @@
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import HomePage from './pages/Home';
-import ProductPage from './pages/Products';
-import RootLayout from './pages/Root';
-import './App.css'
-
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+import Error from "./pages/Error";
+import "./App.css";
 
 const router = createBrowserRouter([
-  {path: '/', 
-  element:<RootLayout/>,
-  children:[
-    {path: '/products', element: <ProductPage/>},
-    {path: '/', element: <HomePage/>}
-  ] 
-  }
-])
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement:<Error />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductPage /> },
+    ],
+  },
+]);
 function App() {
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/pages/Error.jsx
@@ -0,0 +1,12 @@
+import MainNavigation from "../components/MainNavigation";
+
+function Error() {
+    return (
+        <>
+            <MainNavigation />
+            <h1>404 Error Page</h1>
+            <p>Sorry, the page you are looking for does not exist.</p>
+        </>
+    );
+}
+export default Error;
