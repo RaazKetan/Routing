c0a315969733b98ca10c4cfeda2cb22723bc3f59
Raaz <21ketanraaz@gmail.com>
2024-03-26 11:05:24 +0530

    Router 1st approach

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import HomePage from './pages/Home';
+import ProductPage from './pages/Products';
 import './App.css'
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+  {path: '/products', element: <ProductPage/>},
+])
 
 function App() {
   return (
-    <div></div>
+    <RouterProvider router={router}/>
   )
 }
 
